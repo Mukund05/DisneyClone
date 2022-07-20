0d1ff484a3ead0489396d619fa7a06909a75864c
MUKUND THAKUR <92576255+Mukund05@users.noreply.github.com>
2022-07-20 14:06:36 +0530

    fix slider images

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ let movies=[
     {
         name:"Falcon and The Winter Soldier",
         des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. A consectetur illo sint nemo similique impedit fugit? Aliquid unde praesentium voluptatibus, nulla neque culpa, at ut perferendis alias animi error est.",
-        image:"images/slider 2.png"
+        image:"images/slider 2.PNG"
     },
     {
         name:"Loki",
         des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. A consectetur illo sint nemo similique impedit fugit? Aliquid unde praesentium voluptatibus, nulla neque culpa, at ut perferendis alias animi error est.",
-        image:"images/slider 1.png"
+        image:"images/slider 1.PNG"
     },
     {
         name:"Wanda Vision",
         des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. A consectetur illo sint nemo similique impedit fugit? Aliquid unde praesentium voluptatibus, nulla neque culpa, at ut perferendis alias animi error est.",
-        image:"images/slider 3.png"
+        image:"images/slider 3.PNG"
     },
     {
         name:"Raya and the last dragon",
         des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. A consectetur illo sint nemo similique impedit fugit? Aliquid unde praesentium voluptatibus, nulla neque culpa, at ut perferendis alias animi error est.",
-        image:"images/slider 4.png"
+        image:"images/slider 4.PNG"
     },
     {
         name:"luca",
         des:"Lorem ipsum dolor sit amet consectetur adipisicing elit. A consectetur illo sint nemo similique impedit fugit? Aliquid unde praesentium voluptatibus, nulla neque culpa, at ut perferendis alias animi error est.",
-        image:"images/slider 5.png"
+        image:"images/slider 5.PNG"
     }
 
 ];
